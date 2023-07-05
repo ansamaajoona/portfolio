b3cb80699d151587ba12ec9b86967615f3714ac7
Joona <joona.ansa@gmail.com>
2023-07-05 21:42:28 +0300

    tweaks

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -11,13 +11,13 @@ const Footer: React.FC = () => {
             <section id="footer" className="flex flex-row justify-between px-12 pb-5 h-48 bg-gradient-to-b from-slate-800 to-slate-900 items-end">
 
                 <div>
-                    <a href="" target="_blank" className="text-gray-500 font-semibold hover:underline hover:cursor-pointer hover:text-emerald-300 duration-100">
+                    <a href="https://github.com/ansamaajoona/portfolio" target="_blank" className="text-gray-500 font-semibold hover:underline hover:cursor-pointer hover:text-emerald-300 duration-100">
                         The code behind this masterpiece
                     </a>
                 </div>
 
                 <div>
-                    <a href="" target="_blank" className="text-gray-500 font-semibold hover:underline hover:cursor-pointer hover:text-emerald-300 duration-100">
+                    <a href="https://joonaansamaa.dev" target="_blank" className="text-gray-500 font-semibold hover:underline hover:cursor-pointer hover:text-emerald-300 duration-100">
                         &copy; 2023 Joona Ansamaa
                     </a>
                 </div>
